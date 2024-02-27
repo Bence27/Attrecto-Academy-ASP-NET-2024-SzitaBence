82d807235d0c148c7154eb404c756cd6c15c8af2
Bence27 <bence2002327@gmail.com>
2024-02-27 09:45:35 +0100

    Lesson 1

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy_2024.Models
+{
+    public class User
+    {
+        [Required]
+        public int? Id { get; set; }
+
+        [StringLength(10)]
+        public string? FirstName { get; set; }
+
+        [StringLength(10)]
+        public string? LastName { get; set;}
+
+
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using Academy_2024.Models;
+
+namespace Academy_2024.Repositories
+{
+    public class UserRepository
+    {
+        private static List<User> Users = new List<User> { 
+            new User { Id = 1, FirstName = "Jhon", LastName = "Doe" } 
+        };
+
+        public List<User> GetAll() { return Users; }
+
+        public User? GetById(int id)
+        {
+            foreach (var user in Users) 
+            {
+                if(user.Id == id) return user;
+            }
+            return null;
+            
+        }
+
+        public void Create(User data) {
+            Users.Add(data);
+        }
+
+        public User? Update(int id,User data) {
+            foreach (var user in Users)
+            {
+                if (user.Id == id)
+                {
+                    user.FirstName = data.FirstName;
+                    user.LastName = data.LastName;
+                }
+            }
+            return null;
+
+        }
+
+        public bool Delete(int id) 
+        { 
+            foreach (var user in Users) {
+                if (user.Id == id)
+                {
+                    Users.Remove(user);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
